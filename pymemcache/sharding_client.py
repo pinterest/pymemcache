2faf127f05376d0d8acc5a7f8a8fd163e8d1ee13
duanhongyi <duanhongyi@doopai.com>
2015-05-14 17:17:24 +0800

    change doc

--- pymemcache/sharding_client.py
+++ pymemcache/sharding_client.py
@@ -1,14 +1,30 @@
 # -*- coding: utf-8 -*-
+# Copyright 2015 Duan Hongyi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """
-A comprehensive, fast, pure-Python memcached client library.
+A memcached sharding client.
+
 Basic Usage:
 ------------
- from pymemcache.client import Client
- client = Client(('localhost', 11211))
- client.set('some_key', 'some_value')
- result = client.get('some_key')
 
+ from pymemcache.client import PooledClient, ShardingClient
+ c1 = PooledClient(("10.1.15.12", 11211))
+ c2 = PooledClient(("10.1.15.13", 11211))
+ client = ShardingClient([c1, c2])
+ client.set('key', {'a':'b', 'c':'d'})
+ result = client.get('key')
 """
 
 __author__ = "Duan Hongyi"
