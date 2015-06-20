a7c1972dad8d4a1c7df6f22d24a6f3f03d0375fe
John Anderson <sontek@gmail.com>
2015-06-19 20:26:04 -0700

    Move to use pytest instead of nose

--- pymemcache/test/test_utils.py
+++ pymemcache/test/test_utils.py
@@ -1,50 +1,49 @@
-from nose import tools
 import six
+import pytest
 
 from pymemcache.test.utils import MockMemcacheClient
 
 
 def test_get_set():
     client = MockMemcacheClient()
-    tools.assert_equal(client.get(b"hello"), None)
+    assert client.get(b"hello") is None
 
     client.set(b"hello", 12)
-    tools.assert_equal(client.get(b"hello"), 12)
+    assert client.get(b"hello") == 12
 
 
 def test_get_many_set_many():
     client = MockMemcacheClient()
     client.set(b"h", 1)
 
-    tools.assert_equal(client.get_many([b"h", b"e", b"l", b"o"]),
-                       {b"h": 1})
+    result = client.get_many([b"h", b"e", b"l", b"o"])
+    assert result == {b"h": 1}
 
     # Convert keys into bytes
     d = dict((k.encode('ascii'), v)
              for k, v in six.iteritems(dict(h=1, e=2, l=3)))
     client.set_many(d)
-    tools.assert_equal(client.get_many([b"h", b"e", b"l", b"o"]),
-                       d)
+    assert client.get_many([b"h", b"e", b"l", b"o"]) == d
 
 
 def test_add():
     client = MockMemcacheClient()
 
     client.add(b"k", 2)
-    tools.assert_equal(client.get(b"k"), 2)
+    assert client.get(b"k") == 2
 
     client.add(b"k", 25)
-    tools.assert_equal(client.get(b"k"), 2)
+    assert client.get(b"k") == 2
 
 
 def test_delete():
     client = MockMemcacheClient()
 
     client.add(b"k", 2)
-    tools.assert_equal(client.get(b"k"), 2)
+    assert client.get(b"k") == 2
 
     client.delete(b"k")
-    tools.assert_equal(client.get(b"k"), None)
+    assert client.get(b"k") is None
 
 
 def test_incr_decr():
@@ -53,7 +52,7 @@ def test_incr_decr():
     client.add(b"k", 2)
 
     client.incr(b"k", 4)
-    tools.assert_equal(client.get(b"k"), 6)
+    assert client.get(b"k") == 6
 
     client.decr(b"k", 2)
-    tools.assert_equal(client.get(b"k"), 4)
+    assert client.get(b"k") == 4
