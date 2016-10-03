b351fc9e4050821d1ecf2a9fd694b56fb64cfdcf
Suhail Patel <me@suhailpatel.com>
2016-10-03 15:33:41 +0100

    Use cPickle as pickle if available

--- pymemcache/serde.py
+++ pymemcache/serde.py
@@ -12,8 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
 import logging
-import pickle
 from io import BytesIO
 
 try:
