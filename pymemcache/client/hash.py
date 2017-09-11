0745c9ffbb511563a8dad28fdf4d92c149d1de8c
Norman J. Harman Jr <njharman@gmail.com>
2017-09-11 17:50:33 -0500

    Don't Raise Generic Exception (#164)
    Libraries should raise library specific exceptions. Such as
    MemcacheError. This enables users to easily catch exceptions generated
    by library separate from other exceptions. Raising "Exception" forces
    catching Exception and potentially masks all sorts of issues.

--- pymemcache/client/hash.py
+++ pymemcache/client/hash.py
@@ -4,6 +4,7 @@ import logging
 
 from pymemcache.client.base import Client, PooledClient, _check_key
 from pymemcache.client.rendezvous import RendezvousHash
+from pymemcache.exceptions import MemcacheError
 
 logger = logging.getLogger(__name__)
 
@@ -132,7 +133,7 @@ class HashClient(object):
         if server is None:
             if self.ignore_exc is True:
                 return
-            raise Exception('All servers seem to be down right now')
+            raise MemcacheError('All servers seem to be down right now')
 
         client = self.clients[server]
         return client
