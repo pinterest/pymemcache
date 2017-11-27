2b75292d6026978dd5af924b5640a23793741121
Joe Gordon <jogo@pinterest.com>
2017-11-27 11:08:52 -0800

    Test against py35 and py36
    Also fix up travis config to to test flake8 and install missing python
    versions. Previously some tests passed with:
    
        ERROR: InterpreterNotFound: python2.6
        SKIPPED:  py26: InterpreterNotFound: python2.6

--- setup.py
+++ setup.py
@@ -28,6 +28,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: PyPy',
         'License :: OSI Approved :: Apache Software License',
         'Topic :: Database',
