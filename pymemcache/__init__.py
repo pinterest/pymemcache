035fa4fe468f183af68eecdc1a5e0a07db6dc159
unknown <nick.maina1@gmail.com>
2018-11-01 00:19:55 +0300

    Import Classes, Function into package level

--- pymemcache/__init__.py
+++ pymemcache/__init__.py
@@ -3,3 +3,11 @@ __author__ = "Charles Gordon"
 
 from pymemcache.client import Client  # noqa
 from pymemcache.client import PooledClient  # noqa
+
+from pymemcache.exceptions import MemcacheError  # noqa
+from pymemcache.exceptions import MemcacheClientError  # noqa
+from pymemcache.exceptions import MemcacheUnknownCommandError  # noqa
+from pymemcache.exceptions import MemcacheIllegalInputError  # noqa
+from pymemcache.exceptions import MemcacheServerError  # noqa
+from pymemcache.exceptions import MemcacheUnknownError  # noqa
+from pymemcache.exceptions import MemcacheUnexpectedCloseError  # noqa
