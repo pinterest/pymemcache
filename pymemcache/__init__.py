e266168470fa0ea87eb051a59cac89af3588336e
unknown <nick.maina1@gmail.com>
2018-11-01 01:03:25 +0300

    Try fix import error six

--- pymemcache/__init__.py
+++ pymemcache/__init__.py
@@ -1,13 +1,5 @@
 __version__ = '2.0.0'
 __author__ = "Charles Gordon"
 
-from pymemcache.client import Client  # noqa
-from pymemcache.client import PooledClient  # noqa
-
-from pymemcache.exceptions import MemcacheError  # noqa
-from pymemcache.exceptions import MemcacheClientError  # noqa
-from pymemcache.exceptions import MemcacheUnknownCommandError  # noqa
-from pymemcache.exceptions import MemcacheIllegalInputError  # noqa
-from pymemcache.exceptions import MemcacheServerError  # noqa
-from pymemcache.exceptions import MemcacheUnknownError  # noqa
-from pymemcache.exceptions import MemcacheUnexpectedCloseError  # noqa
+from pymemcache.client.base import Client  # noqa
+from pymemcache.client.base import PooledClient  # noqa
