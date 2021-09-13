b515ab6c99a4912fed389c011db0ae425ee3ab44
Tim Gates <tim.gates@iress.com>
2021-09-14 00:43:43 +1000

    docs: Fix a few typos (#348)

--- pymemcache/client/base.py
+++ pymemcache/client/base.py
@@ -289,7 +289,7 @@ class Client:
 
         Args:
           server: tuple(hostname, port) or string containing a UNIX socket path.
-          serde: optional seralizer object, see notes in the class docs.
+          serde: optional serializer object, see notes in the class docs.
           serializer: deprecated serialization function
           deserializer: deprecated deserialization function
           connect_timeout: optional float, seconds to wait for a connection to
