9843ed1280757a80194a5e054ee48dc72582cc1b
Jon Parise <jon@indelible.org>
2025-03-03 08:36:30 -0500

    Remove unused 'mock' variable
    This was causing a lint error:
    
        ./pymemcache/test/test_ext_aws_ec_client.py:41:10: F841 local variable 'mock' is assigned to but never used

--- pymemcache/test/test_ext_aws_ec_client.py
+++ pymemcache/test/test_ext_aws_ec_client.py
@@ -38,7 +38,7 @@ def test_init_valid_node_endpoint(connection_sting, monkeypatch):
 def test_init_invalid_node_endpoint(connection_sting, monkeypatch):
     with patch.object(
         AWSElastiCacheHashClient, "reconfigure_nodes", new=MagicMock()
-    ) as mock:
+    ):
         with pytest.raises(ValueError):
             AWSElastiCacheHashClient(connection_sting, socket_module=MockSocketModule())
 
